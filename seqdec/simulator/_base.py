d5810f752abf9989ab69307e2456e1552588b1ae
raaij <r.t.raaijmakers@student.vu.nl>
2022-09-22 15:50:28 +0200

    Add _ContextualSimulator base class

--- seqdec/simulator/_base.py
+++ seqdec/simulator/_base.py
@@ -1,12 +1,23 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import numpy as np
 
 
-class _SimpleSimulator(ABC):
+class _BaseSimulator(ABC):
     def __init__(self, seed = 12345, *args, **kwargs):
         super().__init__(*args, **kwargs)
         np.random.seed(seed)
-    
+
+
+class _SimpleSimulator(_BaseSimulator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class _ContextualSimulator(_BaseSimulator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
     def observe(self):
-        return None
+        raise NotImplementedError()
